61ba41f10f3a975a84592814383fabc6a97b0511
Vladounett <quentin.charlier55@gmail.com>
2025-11-10 14:24:14 +0100

    flask

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-   
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 def format(data_frame):
     
     data_frame = data_frame.drop([0], axis=1)
@@ -18,8 +21,20 @@ def format(data_frame):
 def scrape(url):
     return pd.read_html(url)[1]
 
+@app.route('/')
+def home():
+    return render_template("index.html")
 
+@app.route('/WSL')
+def wsl_data():
+    df = format(scrape("https://www.soccerdonna.de/en/womens-super-league/startseite/wettbewerb_ENG1.html"))
+    return render_template("league_template.html", tables=[df.to_html(index=False, classes="table table-striped table-bordered")])
+
+@app.route('/PL_FRA')
+def pl_fra_data():
+    df = format(scrape("https://www.soccerdonna.de/en/premire-ligue/startseite/wettbewerb_DAN1.html"))
+    return render_template("league_template.html", tables=[df.to_html(index=False, classes="table table-striped table-bordered")])
 
 if __name__ == '__main__':
-    print(format(scrape("https://www.soccerdonna.de/en/womens-super-league/startseite/wettbewerb_ENG1.html")))
+    app.run(debug=True)
     
